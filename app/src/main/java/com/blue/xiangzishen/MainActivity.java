21bf4947ec69d75cbbfd9622ccca7a126d71f377
bluedai180 <bluedai180@gmail.com>
2016-03-17 23:54:14 +0800

    Add ToolBar, Material Design style.

--- app/src/main/java/com/blue/xiangzishen/MainActivity.java
+++ app/src/main/java/com/blue/xiangzishen/MainActivity.java
@@ -4,12 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toolbar;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.menu_main);
+
     }
 }
